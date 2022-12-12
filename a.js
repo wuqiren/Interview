ba118eaf1a5d342ba9a5b4b83757dd71dbb9dde7
fishfan <fishf3@163.com>
2022-12-12 21:42:39 +0800

    更新react面试题

--- a.js
+++ a.js
@@ -1,5 +1,18 @@
 
-let myname= '极客时间'
-{
-  console.log(myname) 
+let userInfo = {
+  name:"jack.ma",
+  age:13,
+  sex:male,
+  updateInfo:function(){
+    //模拟xmlhttprequest请求延时
+    setTimeout(function(){
+      this.name = "pony.ma"
+      this.age = 39
+      this.sex = female
+    },100)
+  }
 }
+
+userInfo.updateInfo()
+
+console.log(userInfo)
