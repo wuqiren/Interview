9811bfd5ccc10493fb33b8a7a02433daca351c31
fishfan <fishf3@163.com>
2022-12-15 22:22:23 +0800

    浏览器

--- a.js
+++ a.js
@@ -1,9 +1,33 @@
 
-var name= 1;
-var MyObj = {
-  name: 2,
-  showName: function(){
-    console.log(this.name);
-  }
+function executor(resolve, reject) {
+  let rand = Math.random();
+  console.log(1)
+  console.log(rand)
+  if (rand > 0.5)
+      resolve()
+  else
+      reject()
 }
-MyObj.showName()
+var p0 = new Promise(executor); // 1 rand 
+
+var p1 = p0.then((value) => {
+  console.log("succeed-1") // rand>0.5 
+  return new Promise(executor) //1 rand
+})
+
+
+var p3 = p1.then((value) => {
+  console.log("succeed-2") // // rand>0.5 
+  return new Promise(executor)
+})
+
+var p4 = p3.then((value) => {
+  console.log("succeed-3") // rand>0.5 
+  return new Promise(executor)
+})
+
+
+p4.catch((error) => {
+  console.log("error") //// rand<0.5 
+})
+console.log(2)
