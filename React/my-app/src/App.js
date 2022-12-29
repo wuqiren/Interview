680211534d2a45ec4ee3fc8c7709293b61cc675a
fishfan <fishf3@163.com>
2022-12-29 23:38:37 +0800

    redux

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -1,9 +1,26 @@
-import './App.css';
-
+import { useSelector,useDispatch } from "react-redux";
+import {decrement,increment} from './counterSlice'
 function App() {
+  const count = useSelector(state => state.counter.value)
+  const dispatch = useDispatch()
+  console.log(decrement(),'decrementdecrement')
   return (
-    <div className="App">
-      
+    <div className="App" style={{height:'2000px'}}>
+       <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
     </div>
   );
 }
